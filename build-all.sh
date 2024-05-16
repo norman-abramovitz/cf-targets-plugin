6874a704f355b933a65f3bc72c5a5cb3bd3a85d6
Norman Abramovitz <nabramovitz@fivetwenty.io>
2024-05-15 21:38:47 -0700

    1. Imbed semanic versioning information and go os and architecture 2. Use an annotated tag to to maintain version information 3. Started process to create the information needed for cli-plugin-repo

--- build-all.sh
+++ build-all.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+BUILD=$(date -Iseconds -u)
+PRERELEASE="dev"
+DATETAG=$(date -Idate -u)
+echo DATETAG $DATETAG
+VERSION_CORE=$(git describe --tags --abbrev=0 | sed 's/^v//')
+VERSION_CORE=(${VERSION_CORE//./ })
 
 if [[ "$1" = "release" ]] ; then
 	TAG="$2"
@@ -11,44 +18,53 @@ if [[ "$1" = "release" ]] ; then
 	else
 		MAJOR=`echo $TAG | sed 's/^v//' | awk 'BEGIN {FS = "." } ; { printf $1;}'`
 		MINOR=`echo $TAG | sed 's/^v//' | awk 'BEGIN {FS = "." } ; { printf $2;}'`
-		BUILD=`echo $TAG | sed 's/^v//' | awk 'BEGIN {FS = "." } ; { printf $3;}'`
+		PATCH=`echo $TAG | sed 's/^v//' | awk 'BEGIN {FS = "." } ; { printf $3;}'`
 
 		`sed -i .bak -e "s/Major:.*/Major: $MAJOR,/" \
 			-e "s/Minor:.*/Minor: $MINOR,/" \
-			-e "s/Build:.*/Build: $BUILD,/" cf_targets.go`
+			-e "s/Build:.*/Build: $PATCH,/" cf_targets.go`
 	fi
 fi
 
-GOOS=linux GOARCH=amd64 go build
-LINUX64_SHA1=`cat cf-targets-plugin | openssl sha1`
-mkdir -p bin/linux64
-mv cf-targets-plugin bin/linux64
+declare LINUX64_SHA1 OSX_AMD64_SHA1 OSX_ARM64_SHA1 
+function build_for() {
+  echo "Building plugin for $GOOS $GOARCH..."
+  eval "${CHECKSUM}=\"\""
 
-GOOS=darwin GOARCH=amd64 go build
-OSX_AMD64_SHA1=`cat cf-targets-plugin | openssl sha1`
-mkdir -p bin/osx-amd64
-mv cf-targets-plugin bin/osx-amd64
+  VERSION_FLAGS="-X 'main.Major=${VERSION_CORE[0]}'"
+  VERSION_FLAGS="${VERSION_FLAGS} -X 'main.Minor=${VERSION_CORE[1]}'" 
+  VERSION_FLAGS="${VERSION_FLAGS} -X 'main.Patch=${VERSION_CORE[2]}'"  
+  VERSION_FLAGS="${VERSION_FLAGS} -X 'main.PrRls=${PRERELEASE}'"  
+  VERSION_FLAGS="${VERSION_FLAGS} -X 'main.Build=${BUILD}'"  
+  VERSION_FLAGS="${VERSION_FLAGS} -X 'main.GoArch=${GOARCH}'"  
+  VERSION_FLAGS="${VERSION_FLAGS} -X 'main.GoOs=${GOOS}'"
 
-GOOS=darwin GOARCH=arm64 go build
-OSX_ARM64_SHA1=`cat cf-targets-plugin | openssl sha1`
-mkdir -p bin/osx-arm64
-mv cf-targets-plugin bin/osx-arm64/cf-targets-plugin
+  CGO_ENABLED=0 GOARCH=${GOARCH} GOOS=${GOOS} go build -o ./bin/cf-targets-plugin-${GOOS}-${GOARCH} -ldflags "${VERSION_FLAGS}"
+  if [[ $? -eq 0 ]]; then
+    eval "${CHECKSUM}=\"$(openssl sha1 -r bin/cf-targets-plugin-${GOOS}-${GOARCH})\""
+  else
+    eval "${CHECKSUM}=\"cf-targets-plugin-${GOOS}-${GOARCH} failed to compile\""
+  fi
+}
 
-GOOS=windows GOARCH=amd64 go build
-WIN64_SHA1=`cat cf-targets-plugin.exe | openssl sha1`
-mkdir -p bin/win64
-mv cf-targets-plugin.exe bin/win64
+mkdir -p bin
+CHECKSUM="LINUX64_SHA1" GOOS=linux GOARCH=amd64 build_for
+
+CHECKSUM="OSX_AMD64_SHA1" GOOS=darwin GOARCH=amd64 build_for
+
+CHECKSUM="OSX_ARM64_SHA1" GOOS=darwin GOARCH=arm64 build_for
+
+CHECKSUM="WIN64_SHA1" GOOS=windows GOARCH=amd64 build_for
 
 cat repo-index.yml |
-sed "s/osx-amd64-sha1/$OSX_AMD64_SHA1/" |
-sed "s/osx-arm64-sha1/$OSX_ARM64_SHA1/" |
-sed "s/win64-sha1/$WIN64_SHA1/" |
-sed "s/linux64-sha1/$LINUX64_SHA1/" |
-sed "s/_TAG_/$TAG/" |
-cat
+sed -e "s:osx-amd64-sha1:$OSX_AMD64_SHA1:" -e "s:osx-arm64-sha1:$OSX_ARM64_SHA1:" \
+    -e "s:win64-sha1:$WIN64_SHA1:" -e "s:linux64-sha1:$LINUX64_SHA1:" \
+    -e "s:_TAG_:$TAG:" -e "s:_DATE-TAG_:$DATETAG:"
 
 #Final build gives developer a plugin to install
-go build
+localExecutable="bin/cf-targets-plugin-$(go env GOOS)-$(go env GOARCH)"
+rm -f ./cf-targets-plugin 
+[[ -x ${localExecutable} ]] && ln -s ${localExecutable} ./cf-targets-plugin
 
 if [[ "$1" = "release" ]] ; then
 	git commit -am "Build version $TAG"
